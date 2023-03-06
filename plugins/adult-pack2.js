5f1cc2484d13de055c6789933ec115fd972f27b4
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-06 13:17:38 +0100

    Update adult-pack2.js

--- plugins/adult-pack2.js
+++ plugins/adult-pack2.js
@@ -2,48 +2,48 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*'
 let url = packgirl[Math.floor(Math.random() * packgirl.length)]
-conn.sendButton(m.chat, `_🥵 Pack 2 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+conn.sendButton(m.chat, `_🥵 Pack 2 🥵_`, author, url, [['🔄 successivo 🔄', `/cpackf`]], m)
 }
-handler.help = ['pack2']
+handler.help = ['packfabri']
 handler.tags = ['internet']
-handler.command = /^(pack2)$/i
+handler.command = /^(packf)$/i
 export default handler
 
 global.packgirl = [
-  "https://i.imgur.com/mwLJaxU.jpg",
-  "https://i.imgur.com/9ptmlPl.jpg",
-  "https://i.imgur.com/38tVliz.jpg",
-  "https://i.imgur.com/2NqCKE3.jpg",
-  "https://i.imgur.com/pveviMG.jpg",
-  "https://i.imgur.com/d71dnkv.jpg",
-  "https://i.imgur.com/cr7Ypj1.jpg",
-  "https://i.imgur.com/jAxzCj4.jpg",
-  "https://i.imgur.com/xokuFLf.jpg",
-  "https://i.imgur.com/Hi4zLaf.jpg",
-  "https://i.imgur.com/OlaQtwW.jpg",
-  "https://i.imgur.com/Dm4GLuF.jpg",
-  "https://i.imgur.com/k6Y2E9b.jpg",
-  "https://i.imgur.com/1rk7jdu.jpg",
-  "https://i.imgur.com/TFmEVPc.jpg",
-  "https://i.imgur.com/0XefLlJ.jpg",
-  "https://i.imgur.com/bwa9LYZ.jpg",
-  "https://i.imgur.com/WgrpTmg.jpg",
-  "https://i.imgur.com/Z5f5YAw.jpg",
-  "https://i.imgur.com/xEuBtPO.jpg",
-  "https://i.imgur.com/NA0fHxn.jpg",
-  "https://i.imgur.com/InueCKQ.jpg",
-  "https://i.imgur.com/3syOcHe.jpg",
-  "https://i.imgur.com/N1dgels.jpg",
-  "https://i.imgur.com/IxKAJaV.jpg",
-  "https://i.imgur.com/8VrxL1d.jpg",
-  "https://i.imgur.com/8B4Y0bG.jpg",
-  "https://i.imgur.com/wgkGOjF.jpg",
-  "https://i.imgur.com/765Wi6q.jpg",
-  "https://i.imgur.com/5joeWnm.jpg",
-  "https://i.imgur.com/71fjmmM.jpg",
-  "https://i.imgur.com/cAuKeyZ.jpg",
-  "https://i.imgur.com/SDZ2Hs5.jpg",
-  "https://i.imgur.com/skkEyqI.jpg",
-  "https://i.imgur.com/6dXFsBW.jpg",
-  "https://i.imgur.com/6CeG9ZX.jpg"
+  "https://i.imgur.com/MIg1aQr.jpg",
+  "https://i.imgur.com/E2VNhEP.jpg",
+  "https://i.imgur.com/sjb8a36.jpg",
+  "https://i.imgur.com/qUzdTaH.jpg",
+  "https://i.imgur.com/aMHEK6F.jpg",
+  "https://i.imgur.com/AwxQUSc.jpg",
+  "https://i.imgur.com/bk6HQhl.jpg",
+  "https://i.imgur.com/Ukyfti4.jpg",
+  "https://i.imgur.com/Glu7DWp.jpg",
+  "https://i.imgur.com/lkxBrgX.jpg",
+  "https://i.imgur.com/Iij4jSR.jpg",
+  "https://i.imgur.com/BSXR1K8.jpg",
+  "https://i.imgur.com/Iwlkwoz.jpg",
+  "https://i.imgur.com/ghgH9TJ.jpg",
+  "https://i.imgur.com/fWRexee.jpg",
+  "https://i.imgur.com/QSzRuhs.jpg",
+  "https://i.imgur.com/SeB9Sia.jpg",
+  "https://i.imgur.com/QP9m2wn.jpg",
+  "https://i.imgur.com/6K3EmhH.jpg",
+  "https://i.imgur.com/hyhTIZ0.jpg",
+  "https://i.imgur.com/5Cf5lLe.jpg",
+  "https://i.imgur.com/q7lI85A.jpg",
+  "https://i.imgur.com/xZZP8lY.jpg",
+  "https://i.imgur.com/FvbxUfj.jpg",
+  "https://i.imgur.com/efOrZhz.jpg",
+  "https://i.imgur.com/s9fAofv.jpg",
+  "https://i.imgur.com/kBid3ch.jpg",
+  "https://i.imgur.com/GXGYJVe.jpg",
+  "https://i.imgur.com/xerM52P.jpg",
+  "https://i.imgur.com/oefmQJS.jpg",
+  "https://i.imgur.com/KUCRaTd.jpg",
+  "https://i.imgur.com/aKSAwGK.jpg",
+  "https://i.imgur.com/8iJFxic.jpg",
+  "https://i.imgur.com/6v7PbL8.jpg",
+  "https://i.imgur.com/71FDYbw.jpg",
+  "https://i.imgur.com/kWsvvpr.jpg"
 ]
