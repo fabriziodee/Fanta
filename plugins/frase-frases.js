f9a0820b6e6efc1b4079206230fccabbbcffb6b4
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-03-08 21:31:17 +0100

    Update frase-frases.js

--- plugins/frase-frases.js
+++ plugins/frase-frases.js
@@ -14,7 +14,7 @@ try {
 let json = await res.json()
 let { frase } = json
 const tld = 'cn'
-let frase1 = await translate(`${frase}`, { tld, to: 'es' })
+let frase1 = await translate(`${frase}`, { tld, to: 'it' })
 m.reply(`*┏━━━━━━━━━━━━━━━━┓*\n*┠❧  ${frase1}*\n*┗━━━━━━━━━━━━━━━━┚*`)
 } catch {
 m.reply(`*[❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚂𝙸𝙱𝙻𝙴𝙼𝙴𝙽𝚃𝙴 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 (𝙰𝙿𝙸) 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙴𝚂𝚃𝙴 𝙲𝙰𝙸𝙳𝙰 𝚃𝙴𝙼𝙿𝙾𝚁𝙰𝙻𝙼𝙴𝙽𝚃𝙴, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*`)}}
@@ -26,7 +26,7 @@ let res = await fetch("https://supra-api.herokuapp.com/api/romanticafrase?apikey
 let json = await res.json()
 let { frase } = json
 const tld = 'cn'
-let frase1 = await translate(`${frase}`, { tld, to: 'es' })
+let frase1 = await translate(`${frase}`, { tld, to: 'it' })
 m.reply(`*╭─◆────◈⚘◈─────◆─╮*\n*❥  ${frase1}*\n*╰─◆────◈⚘◈─────◆─╯*`)
 } catch {
 m.reply(`*[❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚂𝙸𝙱𝙻𝙴𝙼𝙴𝙽𝚃𝙴 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 (𝙰𝙿𝙸) 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙴𝚂𝚃𝙴 𝙲𝙰𝙸𝙳𝙰 𝚃𝙴𝙼𝙿𝙾𝚁𝙰𝙻𝙼𝙴𝙽𝚃𝙴, 𝙸𝙽𝚃𝙴𝙽𝚃𝙴𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*`)}}
@@ -37,8 +37,8 @@ let res = await fetch("https://api-xcoders.xyz/api/random/cerpen/cinta?apikey=xc
 let json = await res.json()
 let { story, title, author_name } = json.result
 const tld = 'cn'
-let storytime = await translate(`${story}`, { tld, to: 'es' })
-let titletime = await translate(`${title}`, { tld, to: 'es' })
+let storytime = await translate(`${story}`, { tld, to: 'it' })
+let titletime = await translate(`${title}`, { tld, to: 'it' })
 conn.reply(m.chat, `᭥🫐᭢ Título: ${titletime}
 ᭥🍃᭢ Autor: ${author_name}
 ────────────────
